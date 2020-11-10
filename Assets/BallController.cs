ff604c98c18d2a5479f90f4ea7dd4b87837811c5
ankur786-coder <63516933+ankur786-coder@users.noreply.github.com>
2020-11-11 01:16:04 +0530

    Added All Files

---
+++ Assets/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Vector2 startposition, endposition, direction;
+    float startTime, endTime, totalTime;
+    public float force = 0.2f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startposition = Input.GetTouch(0).position;
+            startTime = Time.time;
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endposition = Input.GetTouch(0).position;
+            endTime = Time.time;
+            direction = endposition - startposition;
+            totalTime = endTime - startTime;
+            this.GetComponent<Rigidbody2D>().AddForce(direction / totalTime * force);
+        }
+        
+    }
+}
